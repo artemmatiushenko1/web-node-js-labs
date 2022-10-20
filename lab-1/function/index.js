dfdde52828b8a0fe94132c5634f2374370e4ad84
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-10-20 21:29:54 +0300

    Created azure function

---
+++ lab-1/function/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = async function (context) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  const data = {
+    name: 'Artem',
+    group: 'IP-04',
+  };
+
+  context.res = {
+    status: 200,
+    body: data,
+  };
+};
